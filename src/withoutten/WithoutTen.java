8897cbec1b7a027ce90e23d7e63e8a65c2a80107
ICEARIYA\ariya boonchoo <ariya.bchxxiice@mail.kmutt.ac.th>
2018-02-18 22:26:10 +0700

    [add] test successfull

--- src/withoutten/WithoutTen.java
+++ src/withoutten/WithoutTen.java
@@ -11,33 +11,45 @@ package withoutten;
  */
 public class WithoutTen {
 
-    public static void main(String[] args) {
-        int[] nums = {10, 20, 30, 50, 10, 60, 70};
-//         int [] nums2 = {10,10,20,15,16};
-        withoutTen(nums);
-        String n = ",";
-        int t = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 10) {
-                System.out.print(t);
-                System.out.print(n);
-            }
-
-        }
-    }
+//    public static void main(String[] args) {
+//        int[] nums = {10, 20, 30, 50, 10, 60, 70};
+////         int [] nums2 = {10,10,20,15,16};
+//        withoutTen(nums);
+//        String n = ",";
+//        int t = 0;
+//        for (int i = 0; i < nums.length; i++) {
+//            if (nums[i] == 10) {
+////                System.out.print(t);
+//                nums[i]=t;
+//                System.out.print(nums[i]);
+//                System.out.print(n);
+//            }
+//
+//        }
+//    }
 
     public static int[] withoutTen(int[] nums) {
         int[] arr = new int[nums.length];
         int p = 0;
+        String n = ",";
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 10) {
                 arr[p] = nums[i];
                 System.out.print(arr[p]);
-                String n = ",";
                 System.out.print(n);
                 p++;
 
             }
+        }
+        int t = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 10) {
+//                System.out.print(t);
+                nums[i]=t;
+                System.out.print(nums[i]);
+                System.out.print(n);
+            }
+
         }
 
         return arr;
